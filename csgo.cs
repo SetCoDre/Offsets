002b74a6aa495d9dea80bf805b20ac414aad5161
SetCoDre <46962900+SetCoDre@users.noreply.github.com>
2019-11-21 17:01:04 +0200

    Add files via upload

---
+++ csgo.cs
@@ -0,0 +1,48 @@
+using System;
+
+// 2019-11-21 15:00:56.203305900 UTC
+
+namespace hazedumper
+{
+    public const Int32 timestamp = 1574348456;
+    public static class netvars
+    {
+        public const Int32 m_MoveType = 0x25C;
+        public const Int32 m_aimPunchAngle = 0x302C;
+        public const Int32 m_bIsScoped = 0x3914;
+        public const Int32 m_bSpotted = 0x93D;
+        public const Int32 m_bSpottedByMask = 0x980;
+        public const Int32 m_dwBoneMatrix = 0x26A8;
+        public const Int32 m_fFlags = 0x104;
+        public const Int32 m_hActiveWeapon = 0x2EF8;
+        public const Int32 m_iCrosshairId = 0xB3D4;
+        public const Int32 m_iFOV = 0x31E4;
+        public const Int32 m_iHealth = 0x100;
+        public const Int32 m_iItemDefinitionIndex = 0x2FAA;
+        public const Int32 m_iObserverMode = 0x3374;
+        public const Int32 m_iShotsFired = 0xA380;
+        public const Int32 m_iTeamNum = 0xF4;
+        public const Int32 m_lifeState = 0x25F;
+        public const Int32 m_nTickBase = 0x342C;
+        public const Int32 m_vecOrigin = 0x138;
+        public const Int32 m_vecViewOffset = 0x108;
+    }
+    public static class signatures
+    {
+        public const Int32 dwClientState = 0x588DC4;
+        public const Int32 dwClientState_GetLocalPlayer = 0x180;
+        public const Int32 dwClientState_IsHLTV = 0x4D40;
+        public const Int32 dwClientState_Map = 0x28C;
+        public const Int32 dwClientState_MaxPlayer = 0x388;
+        public const Int32 dwClientState_PlayerInfo = 0x52B8;
+        public const Int32 dwClientState_State = 0x108;
+        public const Int32 dwClientState_ViewAngles = 0x4D88;
+        public const Int32 dwEntityList = 0x4D385AC;
+        public const Int32 dwForceAttack = 0x3169C6C;
+        public const Int32 dwGetAllClasses = 0xD4AC44;
+        public const Int32 dwGlobalVars = 0x588AC8;
+        public const Int32 dwLocalPlayer = 0xD24AAC;
+        public const Int32 dwViewMatrix = 0x4D29FC4;
+        public const Int32 m_bDormant = 0xED;
+    }
+} // namespace hazedumper
