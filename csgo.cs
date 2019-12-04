0c69cffbebce903a7945dabc25b200fd0e6ed546
SetCoDre <46962900+SetCoDre@users.noreply.github.com>
2019-12-04 15:24:59 +0200

    Update csgo.cs

--- csgo.cs
+++ csgo.cs
@@ -1,10 +1,10 @@
 using System;
 
-// 2019-12-01 16:38:46.931441 UTC
+// 2019-12-04 13:24:13.823761800 UTC
 
 namespace hazedumper
 {
-    public const Int32 timestamp = 1575218326;
+    public const Int32 timestamp = 1575465853;
     public static class netvars
     {
         public const Int32 m_aimPunchAngle = 0x302C;
